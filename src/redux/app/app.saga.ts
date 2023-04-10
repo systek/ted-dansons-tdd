0f868116fa73ca364058e338a3bd03ddbfdb85f3
Hans Ole Gjerdrum <hog@eika.no>
2023-04-10 11:33:21 +0200

    [saga]: providing ordernr

--- src/redux/app/app.saga.ts
+++ src/redux/app/app.saga.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { AsyncData } from "@swan-io/boxed";
 import { all, call, put, takeEvery } from "redux-saga/effects";
-import { MenuDTO, OrderDTO, api, paths, orderIdUrl } from "../../api";
+import { MenuDTO, OrderDTO, api, paths, Params } from "../../api";
 import { asyncError, asyncOk } from "../../api/domain/async.utils";
 
 import { menu, moreBeer, order } from "./app.actions";
 import { ActionTypes, Actions } from "./types";
+
+const orderSerializer = (path: string, orderNr: number) =>
+  path.replace(Params.ordernr, `${orderNr}`);
 
 export function* initMenu() {
   yield put(menu({ menu: AsyncData.Loading() }));
@@ -18,15 +21,19 @@ export function* initMenu() {
   }
 }
 
-export function* onPayBill() {
-  yield call(api.del, orderIdUrl);
+export function* onPayBill({ orderNr }: ActionTypes) {
+  yield call(api.del, orderSerializer(paths.order.root, orderNr!));
   yield put(moreBeer());
 }
 
-export function* onOrderInit({ placedOrder }: ActionTypes) {
+export function* onOrderInit({ placedOrder, orderNr }: ActionTypes) {
   yield put(order({ order: AsyncData.Loading() }));
   try {
-    const orderDto: OrderDTO = yield call(api.post, orderIdUrl, placedOrder);
+    const orderDto: OrderDTO = yield call(
+      api.post,
+      orderSerializer(paths.order.root, orderNr!),
+      placedOrder
+    );
 
     yield put(order({ order: asyncOk(orderDto) }));
   } catch (error) {
