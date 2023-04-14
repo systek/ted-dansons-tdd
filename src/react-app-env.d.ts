b0208d91949754d557928b332aab32ea315e5f7b
Hans Ole Gjerdrum <hog@eika.no>
2023-04-14 18:38:08 +0200

    [dispatch-type]: AppDispatchAction

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,6 @@
 /// <reference types="react-scripts" />
 declare type Fn<H, T> = (..._: H) => T;
 declare type DispatchAction<T> = Fn<[T], ActionTypes>;
+declare type Mutable<Type> = {
+  -readonly [Key in keyof Type]: Type[Key];
+};
