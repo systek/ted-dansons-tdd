825f37f46b0324c20e94eaacaf8ac323cd236e4f
Hans Ole Gjerdrum <hog@eika.no>
2023-04-16 09:49:54 +0200

    [selectors]: description

--- src/redux/app/app.selectors.spec.ts
+++ src/redux/app/app.selectors.spec.ts
@@ -2,7 +2,7 @@ import { AppState } from "./types";
 describe("app.selectors", () => {
   it(".....description", () => expect(true).toBeFalsy());
   /* 
-    lag tester for å hente meny (getMenu) og order (getOrder) fra AppState 
-    skriv ulike tester for å håndtere undefined, tomt objekt eller faktisk/forventet verdi
+    lag tester for å hente meny (getMenu) order (getOrder) fra AppState 
+    skriv ulike tester for å håndtere grenseverdier som undefined, tomt objekt eller faktisk/forventet verdi
 */
 });
